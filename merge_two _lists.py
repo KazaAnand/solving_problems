83cd5de009f66a0a027b3644f33a116644ea8e5e
KazaAnand <155720892+KazaAnand@users.noreply.github.com>
2024-01-05 14:12:20 +0530

    Update merge_two _lists.py

--- merge_two _lists.py
+++ merge_two _lists.py
@@ -1,19 +1,4 @@
 
-'''l1=[1,3,4,7]
-l2=[3,4,2,6]
-l3=[]
-for i in range(len(l1)):
-    if l1[i]<l2[i]:
-        l3.append(l1[i])
-        l3.append(l2[i])
-    elif l1[i]>l2[i]:
-        l3.append(l2[i])
-        l3.append(l1[i])
-    else:
-        l3.append(l1[i])
-        l3.append(l2[i])
-
-print(l3)'''
 
 l1=list(map(int,input('l1 nos').split()))
 l2=list(map(int,input('l2 nos').split()))
